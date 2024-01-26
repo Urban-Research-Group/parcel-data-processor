d01ac42b8bb531567d908281278e5e035514a2df
Nicholas Polimeni <nicholas.polimeni@outlook.com>
2024-01-26 08:09:37 -0500

    complete refactoring

--- src/file_utils.py
+++ src/file_utils.py
@@ -1,53 +1,10 @@
 import os
 import pandas as pd
-import numpy as np
+from src import file_io
 from src import operations
-from src.logger import configure_logger, timing
+from src.logger import configure_logger
 
 logger = configure_logger()
-
-
-def _read_format_file(path):
-    return np.loadtxt(path, dtype=str)
-
-
-def _get_widths_from_format(format_file: np.array):
-    return format_file[:, 2].astype(np.int16).tolist()
-
-
-def _get_column_headers_from_format(format_file: np.array):
-    header_col = format_file[:, 0]
-    return header_col
-
-
-def _get_fwf_paramters(format_file_path):
-    format_file = _read_format_file(format_file_path)
-    widths = _get_widths_from_format(format_file)
-    column_headers = _get_column_headers_from_format(format_file)
-    return widths, column_headers
-
-
-def read_var_map(var_map_path: str) -> dict[str, str]:
-    """_summary_
-
-    Args:
-        var_map_path (str): _description_
-
-    Returns:
-        dict[str, str]: _description_
-    """
-    # TODO: derived variables?
-    var_map = pd.read_csv(var_map_path)
-    # we want a dict with (old, source): {new var name: val, type: val, source: val}
-    var_dict = {
-        (row["old_name"], row["source_file"]): {
-            "new_name": row["new_name"],
-            "data_type": row["data_type"],
-        }
-        for _, row in var_map.iterrows()
-        if row["derived"] == "false"
-    }
-    return var_dict
 
 
 def get_all_files_in_dir(directory: str) -> list[str]:
@@ -80,81 +37,8 @@ def select_files(file_paths: list[str], key: str) -> list[str]:
     return [file_path for file_path in file_paths if key in file_path]
 
 
-@timing
-def create_df_from_file(file_path: str, format_file=None) -> pd.DataFrame:
-    """Reads a file based on its extension and returns a dataframe
-
-    Args:
-        file_path (str): path to file
-        format_file_pattern (str, optional): regex pattern for format file, if needed.
-            Defaults to None.
-
-    Raises:
-        ValueError: if file type is not supported
-
-    Returns:
-        pd.DataFrame: dataframe of file
-    """
-    # TODO add and validate file types
-    file_type = file_path.split(".")[-1].lower()
-
-    match file_type:
-        case "csv":
-            df = pd.read_csv(file_path, encoding="latin-1", low_memory=False)
-        case "xlsx" | "xls":
-            df = pd.read_excel(file_path)
-        case "txt":
-            widths, column_headers = _get_fwf_paramters(format_file)
-            df = pd.read_fwf(file_path, widths=widths, header=None, encoding="latin-1")
-            df.columns = column_headers
-        case "dat":
-            NotImplemented
-        case _:
-            raise ValueError(f"File type {file_type} not supported")
-
-    return df
-
-
-def get_desired_cols(file_name: str, var_map: dict) -> list[str]:
-    """Returns a list of all original column names that are desired
-
-    Args:
-        file_name (str): _description_
-        var_map (dict): _description_
-
-    Returns:
-        list[str]: _description_
-    """
-    return [
-        key[0]
-        for key in var_map.keys()
-        if key[1].lower() == "all" or file_name in key[1]
-    ]
-
-
-def get_col_params(column: str, source: str, var_map: dict) -> dict:
-    """_summary_
-
-    Args:
-        column (str): _description_
-        source (str): _description_
-        var_map (dict): _description_
-
-    Returns:
-        dict: _description_
-    """
-    # TODO: might want to convert data type strings to actual types here
-    params = None
-
-    for key in var_map.keys():
-        if key[0] == column and (source in key[1] or key[1].lower() == "all"):
-            params = var_map[key]
-
-    return params
-
-
 def create_dfs_from_files(
-    file_paths: list[str], format_file: str = None, var_map: dict = None
+    file_paths: list[str], format_file: str = None, var_map: pd.DataFrame = None
 ) -> list[pd.DataFrame]:
     """Reads in a file as a DataFrame and performs data cleaning operations for
     each file in file_paths
@@ -176,13 +60,13 @@ def create_dfs_from_files(
     for file_path in file_paths:
         file_name = file_path.split("\\")[-1]
 
-        df = create_df_from_file(file_path, format_file)
-        logger.info(f"Shape of {file_name} when read: {df.shape}")
+        df = file_io.File(file_path, format_file).read()
+        logger.info("Shape of %s when read: %s", file_name, df.shape)
 
         df = operations.clean_df(df, file_name, var_map)
         df = operations.create_derived_cols(df, file_name)
 
-        logger.info(f"Shape of {file_name} after processing: {df.shape}")
+        logger.info("Shape of %s after processing: %s", file_name, df.shape)
         dfs.append(df)
 
     return dfs
---
+++ src/processor.py
@@ -0,0 +1,106 @@
+import pandas as pd
+from src import operations
+from src import file_utils
+from src.config import DataInfo
+from src.logger import configure_logger, timing
+
+logger = configure_logger()
+
+
+class DataProcessor:
+    def __init__(self, data_info: DataInfo):
+        self.data_info = data_info
+        self.county_files = data_info.county_files
+        self.var_map_non_derived = data_info.var_map_non_derived
+        self.var_map_derived = data_info.var_map_derived
+        self.operations = data_info.operations
+        self.data_per_op = {name: [] for name in self.operations}
+        self.current_op = None
+
+    @timing
+    def load_data(self, file_pattern: str, format_file: str) -> list[pd.DataFrame]:
+        """Selects required files and calls on file_utils to complete I/O read
+
+        Args:
+            data_info (data_info): dataclass providing data instruction parameters
+            file_pattern (str): selects files matching this pattern in given dir path
+            format_file (str): pattern to identify format files for each data file, if needed
+
+        Returns:
+            list[pd.DataFrame]: list of dataframes created by reading in each file specified
+        """
+        data = None
+
+        if file_pattern not in self.operations:
+            selected_files = file_utils.select_files(self.county_files, file_pattern)
+
+            data = file_utils.create_dfs_from_files(
+                selected_files, format_file, self.var_map_non_derived
+            )
+        else:
+            data = self.data_per_op[file_pattern]
+
+        return data
+
+    @staticmethod
+    def guard_join(operation) -> None:
+        if "key" not in operation or "join-type" not in operation:
+            error_msg = "Key or merge type not specified for join operation"
+            logger.error(error_msg)
+            raise ValueError(error_msg)
+
+    @timing
+    @staticmethod
+    def process_operation(operation, data, var_map_derived) -> pd.DataFrame:
+        """Executes a single data operation as specified by the data info params
+
+        Args:
+            operation (dict): operation params from data info params
+            data (list[pd.DataFrame]): data to execute the operation on
+
+        Raises:
+            ValueError: if a merge is specified without a key or merge-type
+
+        Returns:
+            pd.DataFrame: result of executing the operation on the given data
+        """
+        operation_type = operation["type"]
+
+        match operation_type:
+            case "append" | "concat":
+                return operations.concat(data)
+            case "merge" | "join":
+                DataProcessor.guard_join(operation)
+                return operations.join(data, operation["key"], operation["join-type"])
+            case "derive_vars":  # TODO do we just want this as a param
+                return operations.derive_vars(data, var_map_derived)
+            case _:
+                print("Operation not supported")
+
+    @timing
+    def run(self) -> pd.DataFrame:
+        """Executes, in order, every operation specified in the config
+
+        Args:
+            data_info (data_info): dataclass providing operation instructions
+
+        Returns:
+            pd.DataFrame: end result of executing the operations
+        """
+        for name, operation in self.operations.items():
+            files = operation["files"]
+
+            data = [
+                self.load_data(file_pat, format_file)
+                for file_pat, format_file in files.items()
+            ]
+
+            self.data_per_op[name] = DataProcessor.process_operation(
+                operation, data, self.var_map_derived
+            )
+            self.current_op = name
+
+        result = self.data_per_op[self.current_op]
+        logger.info("Shape of Result: %s", result.shape)
+
+        return result
