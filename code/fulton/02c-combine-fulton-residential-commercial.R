bb1d28760a9bb5500f1911b4a097e8448e9b8815
Melissa Juarez <mj2945@columbia.edu>
2025-08-18 19:33:09 -0400

    fix paths and minor code improvements

--- code/fulton/02c-combine-fulton-residential-commercial.R
+++ code/fulton/02c-combine-fulton-residential-commercial.R
@@ -8,19 +8,23 @@ library(readxl)
 library(janitor)
 library(data.table)
 
-## NF, SF, and DIGEST files cleaned
-nfsf_digest <- read_csv(here("output/DIGEST_fulton_NFSFDIGEST_2011_2022_apr092025.csv"))
+## NF, SF, and DIGEST files cleaned -- need to filter out 2010 since CMR digest does not cover that year
+nfsf_digest <- read_csv(here::here("output/fulton/2-processed_clean/DIGEST_fulton_NFSFDIGEST_2010_2022_aug182025.csv")) %>%
+  filter(taxyr != 2010)
 
 nfsf_digest <- nfsf_digest %>%
-  dplyr::rename(yrblt = d_yrblt,
+  rename(yrblt = d_yrblt,
+         effyr = d_effyr,
          sf = sfla,
-         grade = d_grade)
+         grade = d_grade,
+         card = d_card,
+         cdu = d_cdu,
+         pctcomp = d_pctcomplete)
 
 ## need to bring this in because ns/sf does not contain all commercial data
 ## no vars have been dropped from raw data in this
 ## commercial data does not have unit number in the notes section
-commercial_digest <- read_excel(here::here("output/CMR/DIGEST_fulton_commercial_2011_2022_feb202025.xlsx")) %>%
-  clean_names()
+commercial_digest <- read_csv(here::here("output/fulton/2-processed_clean/CMR/DIGEST_fulton_commercial_2011_2022_aug182025.csv"))
 
 ############## Column name comparisons ############## 
 ## get col names from each file
@@ -49,19 +53,20 @@ column_presence
 
 keep_columns <- c(common_columns, "note1", "note2")
 
-############## Bind similar rows only & isolate ownership info ############## 
+############## Bind similar columns only #######################################
 library(plyr)
 fulton_all <- rbind.fill(commercial_digest[common_columns], 
                     nfsf_digest[keep_columns]) %>%
   distinct()
 
 ## remove duplicates of parcel ids
-fulton_all_owner <- data.table(fulton_all)
-fulton_all_owner <- fulton_all_owner[order(-sf), .SD[1], by = .(taxyr, parid)]
-fulton_all_owner <- fulton_all_owner[order(taxyr, parid)]
+fulton_all <- data.table(fulton_all)
+fulton_all <- fulton_all[order(-sf), .SD[1], by = .(taxyr, parid)]
+fulton_all <- fulton_all[order(taxyr, parid)]
 
+cat("There is only one row per (taxyr, parid) group:", fulton_all[, .N, by = .(taxyr, parid)][, max(N) == 1])
 
-write_csv(fulton_all_owner, file = here::here("output/fulton_ALL_ownership_2011_2022.csv"))
+write_csv(fulton_all, file = here::here("output/fulton/2-processed_clean/DIGEST_fulton_2011_2022.csv"))
 
 
 
